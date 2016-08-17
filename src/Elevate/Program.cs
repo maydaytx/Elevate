f2d8d23cf841ee954e0d1cd70033e233d7126e92
Brandon Barry <maydaytx@gmail.com>
2016-08-17 16:46:57 -0500

    added code

---
+++ src/Elevate/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Elevate
+{
+    internal static class Program
+    {
+        private static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("USAGE: elevate command [args...]");
+                return 1;
+            }
+
+            var command = args[0];
+            var arguments = string.Join(" ", args.Skip(1));
+
+            if (command.ToLower() == "start")
+            {
+                command = "cmd";
+                arguments = "/C \"cd /D " + Environment.CurrentDirectory +" && start " + arguments + "\"";
+            }
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = command,
+                    Arguments = arguments,
+                    UseShellExecute = true,
+                    Verb = "runas"
+                }
+            };
+
+            try
+            {
+                process.Start();
+
+                process.WaitForExit();
+            }
+            catch
+            {
+                return 1;
+            }
+
+            return process.ExitCode;
+        }
+    }
+}
---
+++ src/Test/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using Microsoft.Win32;
+
+namespace Test
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var isElevated = UacHelper.IsProcessElevated;
+            var workingDirectory = Environment.CurrentDirectory;
+            Debugger.Launch();
+        }
+    }
+
+    internal static class UacHelper
+    {
+        private const string uacRegistryKey = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
+        private const string uacRegistryValue = "EnableLUA";
+
+        private static uint STANDARD_RIGHTS_READ = 0x00020000;
+        private static uint TOKEN_QUERY = 0x0008;
+        private static uint TOKEN_READ = (STANDARD_RIGHTS_READ | TOKEN_QUERY);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool OpenProcessToken(IntPtr ProcessHandle, UInt32 DesiredAccess, out IntPtr TokenHandle);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool GetTokenInformation(IntPtr TokenHandle, TOKEN_INFORMATION_CLASS TokenInformationClass, IntPtr TokenInformation, uint TokenInformationLength, out uint ReturnLength);
+
+        public enum TOKEN_INFORMATION_CLASS
+        {
+            TokenUser = 1,
+            TokenGroups,
+            TokenPrivileges,
+            TokenOwner,
+            TokenPrimaryGroup,
+            TokenDefaultDacl,
+            TokenSource,
+            TokenType,
+            TokenImpersonationLevel,
+            TokenStatistics,
+            TokenRestrictedSids,
+            TokenSessionId,
+            TokenGroupsAndPrivileges,
+            TokenSessionReference,
+            TokenSandBoxInert,
+            TokenAuditPolicy,
+            TokenOrigin,
+            TokenElevationType,
+            TokenLinkedToken,
+            TokenElevation,
+            TokenHasRestrictions,
+            TokenAccessInformation,
+            TokenVirtualizationAllowed,
+            TokenVirtualizationEnabled,
+            TokenIntegrityLevel,
+            TokenUIAccess,
+            TokenMandatoryPolicy,
+            TokenLogonSid,
+            MaxTokenInfoClass
+        }
+
+        public enum TOKEN_ELEVATION_TYPE
+        {
+            TokenElevationTypeDefault = 1,
+            TokenElevationTypeFull,
+            TokenElevationTypeLimited
+        }
+
+        public static bool IsUacEnabled
+        {
+            get
+            {
+                var uacKey = Registry.LocalMachine.OpenSubKey(uacRegistryKey, false);
+                var result = uacKey.GetValue(uacRegistryValue).Equals(1);
+
+                return result;
+            }
+        }
+
+        public static bool IsProcessElevated
+        {
+            get
+            {
+                if (IsUacEnabled)
+                {
+                    IntPtr tokenHandle;
+                    if (!OpenProcessToken(Process.GetCurrentProcess().Handle, TOKEN_READ, out tokenHandle))
+                    {
+                        throw new ApplicationException("Could not get process token.  Win32 Error Code: " + Marshal.GetLastWin32Error());
+                    }
+
+                    var elevationResult = TOKEN_ELEVATION_TYPE.TokenElevationTypeDefault;
+
+                    var elevationResultSize = Marshal.SizeOf((int)elevationResult);
+                    var elevationTypePtr = Marshal.AllocHGlobal(elevationResultSize);
+
+                    uint returnedSize;
+                    if (!GetTokenInformation(tokenHandle, TOKEN_INFORMATION_CLASS.TokenElevationType, elevationTypePtr, (uint)elevationResultSize, out returnedSize))
+                        throw new ApplicationException("Unable to determine the current elevation.");
+
+                    elevationResult = (TOKEN_ELEVATION_TYPE)Marshal.ReadInt32(elevationTypePtr);
+                    var isProcessAdmin = elevationResult == TOKEN_ELEVATION_TYPE.TokenElevationTypeFull;
+
+                    return isProcessAdmin;
+                }
+
+                var identity = WindowsIdentity.GetCurrent();
+                var principal = new WindowsPrincipal(identity);
+                var result = principal.IsInRole(WindowsBuiltInRole.Administrator);
+
+                return result;
+            }
+        }
+    }
+}
